e31e5b14dec91bed7032436934c9c5bc3db44124
JD <jdfili14@gmail.com>
2019-02-09 21:19:34 -0800

    match works, fix modal...

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,26 @@ module.exports = function (app) {
         res.json(tableData);
     });
     app.post("/api/friends", function (req, res) {
-        tableData.push(req.body);
-});
+        var match = {
+            name: "",
+            photo: "",
+            difference: 1000
+        };
+        var userScore = req.body.score;
+        var totalDifference = 0;
+        for (var i = 0; i < tableData.length; i++) {
+            totalDifference = 0;
+            for (var n = 0; n < tableData[i].score[n]; n++) {
+                totalDifference += Math.abs(parseInt(userScore[n] - tableData[i].score[n]))
+                if (totalDifference <= match.difference) {
+                    match.name = tableData[i].name;
+                    match.photo = tableData[i].image;
+                    match.difference = totalDifference;
+
+                }
+            }
+        }
+        res.json(match);
+    });
 
 };
