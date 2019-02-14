6d3125030d43945c08c25200852e22674972e9a8
JD <jdfili14@gmail.com>
2019-02-13 16:49:11 -0800

    added friends, error handling

--- app/data/friends.js
+++ app/data/friends.js
@@ -11,7 +11,17 @@ var tableArray = [
         name: "Buckethead",
         image: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2017/10/GettyImages-72291000-920x584.jpg",
         score: ["3", "1", "3", "3", "4", "1", "1", "3", "5", "5"]
+    }, {
+        name: "Sam Cooke",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Sam_Cooke_2.jpg/220px-Sam_Cooke_2.jpg",
+        score: ["3", "3", "2", "5", "2", "3", "3", "2", "3", "4"]
+    }, {
+        name: "Charlie Kelly",
+        image: "https://pbs.twimg.com/profile_images/809639890111647744/ueKVKgvu.jpg",
+        score: ["4", "4", "2", "1", "3", "2", "3", "4", "3", "2"]
     }
+
+
 ];
 
 module.exports = tableArray;
